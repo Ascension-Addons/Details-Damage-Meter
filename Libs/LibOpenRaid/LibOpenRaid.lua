4c65968927693342038809fbfec3b6b6ce4f2344
Tercio Jose <terciob19@hotmail.com>
2024-07-30 12:59:00 -0300

    Fixed /key not showing the map name

--- Libs/LibOpenRaid/LibOpenRaid.lua
+++ Libs/LibOpenRaid/LibOpenRaid.lua
@@ -31,7 +31,7 @@ LIB_OPEN_RAID_CAN_LOAD = false
 
 local major = "LibOpenRaid-1.0"
 
-local CONST_LIB_VERSION = 132
+local CONST_LIB_VERSION = 133
 
 if (LIB_OPEN_RAID_MAX_VERSION) then
     if (CONST_LIB_VERSION <= LIB_OPEN_RAID_MAX_VERSION) then
